f5bb005293dd7cc5f9921d9e2b04b08633c7f27b
master-kill <57908145+master-kill@users.noreply.github.com>
2025-07-04 22:44:46 +0300

    Update bot_handler.py

--- bot_handler.py
+++ bot_handler.py
@@ -2,9 +2,18 @@ import os
 from mqtt_handler import latest_status
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler
+from mqtt_handler import last_status
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
+
+@bot.message_handler(commands=['status'])
+def status_handler(message):
+    if last_status:
+        bot.send_message(message.chat.id, last_status, parse_mode="Markdown")
+    else:
+        bot.send_message(message.chat.id, "⚠️ Данных ещё нет.")
+
 
 def status_command(update, context):
     if latest_status:
