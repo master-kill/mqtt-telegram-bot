9978d375df6bd877a021b2ee1a713e6c30acc72f
master-kill <57908145+master-kill@users.noreply.github.com>
2025-07-06 13:46:10 +0300

    Update data_store.py

--- Test/data_store.py
+++ Test/data_store.py
@@ -2,9 +2,17 @@
 
 latest_data = None
 
+user_subscriptions = {
+    5335196591: ['Carlsberg'],
+    1234567890: ['UPS-1', 'Carlsberg']
+}
+
 def set_latest_data(data):
     global latest_data
     latest_data = data
 
 def get_latest_data():
     return latest_data
+
+def get_user_subscriptions(chat_id):
+    return user_subscriptions.get(chat_id, [])
