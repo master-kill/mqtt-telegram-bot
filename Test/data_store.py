cc18a089b55250ae67e50dc7c9604d764fab5942
master-kill <57908145+master-kill@users.noreply.github.com>
2025-07-08 19:56:52 +0300

    Update data_store.py

--- Test/data_store.py
+++ Test/data_store.py
@@ -1,18 +1,7 @@
 # data_store.py
 
-latest_data = None
+# Последние данные по каждому устройству
+latest_data = {}  # device_id -> payload
 
-user_subscriptions = {
-    5335196591: ['Carlsberg'],
-    1234567890: ['UPS-1', 'Carlsberg']
-}
-
-def set_latest_data(data):
-    global latest_data
-    latest_data = data
-
-def get_latest_data():
-    return latest_data
-
-def get_user_subscriptions(chat_id):
-    return user_subscriptions.get(chat_id, [])
+# Подписки: chat_id -> set(device_ids)
+subscriptions = {}
